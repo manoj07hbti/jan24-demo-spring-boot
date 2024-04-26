f367200de53be370391a2d0f92bedc7505d6bc2a
Manoj Kumar <manojkumar.pcf@gmail.com>
2024-04-26 20:58:22 +0530

    Day 1 Controller class

---
+++ src/main/java/com/example/jan24demospringboot/Jan24DemoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.jan24demospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jan24DemoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jan24DemoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jan24demospringboot/Jan24DemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jan24demospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jan24DemoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
