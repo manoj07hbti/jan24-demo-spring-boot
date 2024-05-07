d2fa8567cbe6754d2602076c429774e55e120d44
zameer0516 <zameers78600@gmail.com>
2024-05-07 11:58:13 +0530

    Controller_endpoint Assignment

--- src/main/java/com/example/jan24demospringboot/demo_controller/Controller_1.java
+++ src/main/java/com/example/jan24demospringboot/demo_controller/Controller_1.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller_1 {
 
     @RequestMapping("/square")
-    public String make_square(){
-        int num=7;
-        return num*num*num +"square of given number";
+    public String make_square() {
+        int num = 25;
+        return num * num  + "  is square of given number";
     }
+
+
+
 
 
 }
