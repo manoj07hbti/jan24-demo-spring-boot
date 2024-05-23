e718dde1415b2d9b7cc2350067919ddbe5c696ed
Manoj Kumar <manojkumar.pcf@gmail.com>
2024-05-23 20:55:55 +0530

    DB Table and App architecture

---
+++ src/main/java/com/example/jan24demospringboot/controller/AppArchController.java
@@ -0,0 +1,23 @@
+package com.example.jan24demospringboot.controller;
+
+import com.example.jan24demospringboot.service.AppArchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppArchController {
+
+    @Autowired
+    AppArchService service;
+
+    @RequestMapping("/app_hello")
+    public String hello(){
+
+      //  AppArchService obj= new AppArchService();
+
+      return   service.hello();
+
+    }
+
+}
--- src/main/java/com/example/jan24demospringboot/model/Student.java
+++ src/main/java/com/example/jan24demospringboot/model/Student.java
@@ -1,10 +1,25 @@
 package com.example.jan24demospringboot.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "STUDENT_JAN24_JAVA") // it will consider this class as db table
 public class Student {
 
+    @Id //it will make your field as primary key
+    @GeneratedValue // it will auto increment id filed in database
+    long id;
+
     String name;
+    @Column(name="roll_no")
     int rollNo;
     String section;
+// default constructor
+
+    public Student() {
+    }
 
     public Student(String name, int rollNo, String section) {
         this.name = name;
