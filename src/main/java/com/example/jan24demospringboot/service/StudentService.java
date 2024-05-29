3728eb2e7569f6ab9a241cd7f839205f7f1efbd4
Manoj Kumar <manojkumar.pcf@gmail.com>
2024-05-29 21:16:19 +0530

    DB crud

--- src/main/java/com/example/jan24demospringboot/service/StudentService.java
+++ src/main/java/com/example/jan24demospringboot/service/StudentService.java
@@ -5,6 +5,8 @@ import com.example.jan24demospringboot.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 public class StudentService {
@@ -15,5 +17,29 @@ public class StudentService {
 
         repository.save(student);// INSERT INTO STUDENT ...
         return "Student added  to the DB";
+    }
+
+    public List<Student> getAllStudent(){
+
+        return repository.findAll();// select * from table
+    }
+
+    public String updateName(String newName, long id){
+
+         // find record from db
+       Student student=   repository.getById(id);  //select * from STUDENT_JAN24_JAVA where id=101
+         // update the name with the help of setter method
+        student.setName(newName);
+        // save back to the database again
+        repository.save(student);
+
+        return "Name " +newName+ "updates successfully ";
+    }
+
+    public String removeStudent(long id){
+
+        repository.deleteById(id);// delete from table where id=
+
+        return "Data removed successfully...";
     }
 }
